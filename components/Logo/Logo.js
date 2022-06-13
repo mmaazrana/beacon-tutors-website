a72cb09296d136aff2a53fe20b94729d0c5cb3e0
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-06-13 11:37:57 +0500

    used Image for logo

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,11 +1,10 @@
-// import React from 'react';
-// import Image from 'next/image'
-// import logo from '../../assets/beacon-tutors.svg'
-// export default function Logo(props) {
-//     return (
-//         <>
-//             <Image src={logo}  />
-//         </>
-//     );
-// }
-
+import React from 'react';
+import Image from 'next/image';
+import logo from '../../assets/beacon-tutors.svg';
+export default function Logo(props) {
+  return (
+    <>
+      <Image src={logo} />
+    </>
+  );
+}
--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,34 +1,29 @@
 import React from 'react';
-// import Logo from "../Logo/Logo";
-import Logo from '../../assets/beacon-tutors.svg';
-import Link from "next/link";
-import styles from './Nav.module.css'
-import SecondaryButton from '../Buttons/SecondaryButton'
-import PrimaryButton from "../Buttons/PrimaryButton";
+import Logo from '../Logo/Logo';
+import Link from 'next/link';
+import styles from './Nav.module.css';
+import SecondaryButton from '../Buttons/SecondaryButton';
+import PrimaryButton from '../Buttons/PrimaryButton';
 export default function Nav(props) {
-    return (
-        <>
-            <div className={styles.nav} >
-
-            <div className={styles.logo} >
-            <Logo />
-            <Link href={"./"}>
-                Logo
-            </Link>
-            </div>
-            <div className={styles.buttons} >
-                <div className={styles.buttonsSecondary} >:
-                    <SecondaryButton text={'Home'} />
-                    <SecondaryButton text={'Our Services'} />
-                    <SecondaryButton text={'Work as Tutor'} />
-                    <SecondaryButton text={'Terms & Conditions'} />
-                </div>
-                <PrimaryButton text={'Submit Inquiry'} />
-            </div>
-
-            </div>
-
-        </>
-    );
+  return (
+    <>
+      <div className={styles.nav}>
+        <div className={styles.logo}>
+          <Logo />
+          <Link href={'./'}>Logo</Link>
+        </div>
+        <div className={styles.buttons}>
+          <div className={styles.buttonsSecondary}>
+            :
+            <SecondaryButton text={'Home'} />
+            <SecondaryButton text={'Our Services'} />
+            <SecondaryButton text={'Work as Tutor'} />
+            <SecondaryButton text={'Terms & Conditions'} />
+          </div>
+          <PrimaryButton text={'Submit Inquiry'} />
+        </div>
+      </div>
+    </>
+  );
 }
 
