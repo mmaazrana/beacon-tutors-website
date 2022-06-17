29a14c773408abe89daa6c55e6a66f8153a13d3d
mmaazrana <awaismaaz@gmail.com>
2022-06-17 11:51:43 +0500

    completed how it works, added advatnages section, made all sections fully responsive, fixed bugs

--- components/Working/Working.js
+++ components/Working/Working.js
@@ -1,22 +1,33 @@
 import styles from './Working.module.css';
+import {ReactComponent as RightArrow} from '../../assets/rightArrow.svg'
+import {ReactComponent as LeftArrow} from '../../assets/leftArrow.svg'
 
 export default function Working(props) {
-  return (
-    <div
-      className={`${styles.working} ${props.index % 2 === 1 && styles.right}`}
-    >
-      <div
-        className={`${styles.content} ${props.index % 2 === 1 && styles.right}`}
-      >
-        <div>Illustration</div>
-        <div>
-          <h4>Submit Your Inquiry</h4>
-          <p>
-            Send us a brief description of your requirements by contacting on
-            our official WhatsApp account or by giving us a call.
-          </p>
+    return (<>
+        <div
+            className={`${styles.working} ${props.index % 2 === 1 && styles.rightWorking}`}
+        >
+            <div
+                className={`${styles.content} ${props.index % 2 === 1 && styles.rightContent}`}
+            >
+
+                <div className={styles.image}>
+                    {props.image}
+                </div>
+                <div className={styles.text}>
+                    <h4 className={styles.heading}>{props.heading}</h4>
+                    <p className={styles.description}>
+                        {props.description}
+                    </p>
+                </div>
+
+            </div>
+            {props.index!== props.length-1 &&
+            <div className={`${styles.arrowWrapper} ${props.index % 2 === 1 && styles.rightWrapper}`}>
+                {props.index % 2 === 1 ? <RightArrow className={styles.arrow}/> : <LeftArrow className={styles.arrow}/>}
+            </div>
+            }
+
         </div>
-      </div>
-    </div>
-  );
+    </>);
 }
