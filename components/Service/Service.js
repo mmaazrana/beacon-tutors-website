c8c181ac99e38c1879bb7be5bd583e501505bfe0
mmaazrana <awaismaaz@gmail.com>
2022-06-18 14:08:57 +0500

    added useRouter conditonal in services

--- components/Service/Service.js
+++ components/Service/Service.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import LinkButton from "../Buttons/LinkButton";
 import styles from "./Service.module.css"
+import {useRouter} from "next/router";
+
 export default function Service(props) {
-    return (
-        <>
-            <div className={styles.main} >
-                <div className={styles.image} >
+
+    const {pathName} = useRouter();
+
+    return (<>
+            <div className={styles.main}>
+                <div className={styles.image}>
                     {props.image}
-                </div >
-                <div className={styles.text} >
-                    <h4 className={styles.heading} >
+                </div>
+                <div className={styles.text}>
+                    <h4 className={styles.heading}>
                         {props.heading}
                     </h4>
-                    <p className={styles.description} >
+                    <p className={styles.description}>
                         {props.description}
                     </p>
-                    <LinkButton primary={true} text={"Learn More"}  link={props.link} />
+                    {pathName === '/' && <LinkButton primary={true} text={"Learn More"} link={props.link}/>}
                 </div>
             </div>
-        </>
-    );
+        </>);
 }
 
