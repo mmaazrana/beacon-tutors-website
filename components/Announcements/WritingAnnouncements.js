8e05d9d4fc0d69320191c18f7d434f89e98f0788
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-06-19 19:43:53 +0500

    created edit announcement modal and separated reusable code and styles for forms

--- components/Announcements/WritingAnnouncements.js
+++ components/Announcements/WritingAnnouncements.js
@@ -1,35 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import styles from './Announcements.module.css';
 import Announcement from '../Announcement/Announcement';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
 
-const data = [
+const initialData = [
   {
-    title: 'Need an experienced female tutor for O-level Biology',
+    title: 'Announcement 1',
     days: '5-Days / Week',
     budget: '20,000 PKR / Month',
     time: '1-Hour / Day',
   },
   {
-    title: 'Need an experienced female tutor for O-level Biology',
+    title: 'Announcement 2',
     days: '5-Days / Week',
     budget: '20,000 PKR / Month',
     time: '1-Hour / Day',
   },
   {
-    title: 'Need an experienced female tutor for O-level Biology',
+    title: 'Announcement 2',
     days: '5-Days / Week',
     budget: '20,000 PKR / Month',
     time: '1-Hour / Day',
   },
   {
-    title: 'Need an experienced female tutor for O-level Biology',
-    days: '5-Days / Week',
-    budget: '20,000 PKR / Month',
-    time: '1-Hour / Day',
-  },
-  {
-    title: 'Need an experienced female tutor for O-level Biology',
+    title: 'Announcement 2',
     days: '5-Days / Week',
     budget: '20,000 PKR / Month',
     time: '1-Hour / Day',
@@ -37,30 +33,160 @@ const data = [
 ];
 
 export default function WritingAnnouncements(props) {
-  const [announcements, setAnnouncements] = useState(data);
+  const [count, setCount] = useState(0);
+  const [announcements, setAnnouncements] = useState(initialData);
+
+  const settings = {
+    vertical: true,
+    verticalSwiping: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    pauseOnFocus: true,
+    swipeToSlide: true,
+    focusOnSelect: true,
+    centerMode: true,
+  };
+
+  useEffect(() => {
+    console.log(announcements);
+  }, [announcements]);
+
+  const populateData = () => {
+    return [
+      {
+        title: `Load More ${count}`,
+        days: '5-Days / Week',
+        budget: '20,000 PKR / Month',
+        time: '1-Hour / Day',
+      },
+      {
+        title: `Load More ${count}`,
+        days: '5-Days / Week',
+        budget: '20,000 PKR / Month',
+        time: '1-Hour / Day',
+      },
+    ];
+  };
 
   const fetchMoreData = () => {
     setTimeout(() => {
-      setAnnouncements([...announcements, ...data]);
+      setAnnouncements([
+        ...announcements,
+        ...populateData(setCount(count + 1)),
+      ]);
     }, 500);
   };
 
   return (
     <div
       className={styles.announcements}
-      style={{ margin: '0 auto', width: '70%' }}
+      id="scrollableDiv"
+      style={{ height: 250, overflow: 'auto' }}
     >
       <InfiniteScroll
         dataLength={announcements.length}
         next={fetchMoreData}
         hasMore={true}
-        height={500}
+        scrollableTarget="scrollableDiv"
+      >
+        <Slider
+          {...settings}
+        >
+          {announcements.map((announcement, index) => (
+            <Announcement key={index} announcement={announcement} />
+          ))}
+        </Slider>
+      </InfiniteScroll>
+
+      {/* <InfiniteScroll
+        className={styles.list}
+        dataLength={announcements.length}
+        next={fetchMoreData}
+        hasMore={true}
         loader={<h4>Loading...</h4>}
+        height={400}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
       >
         {announcements.map((announcement, index) => (
           <Announcement key={index} announcement={announcement} />
         ))}
-      </InfiniteScroll>
+      </InfiniteScroll> */}
     </div>
   );
 }
+
+// import React, { useState } from 'react';
+// import InfiniteScroll from 'react-infinite-scroll-component';
+// import styles from './Announcements.module.css';
+// import Announcement from '../Announcement/Announcement';
+
+// const data = [
+//   {
+//     title: 'Need an experienced female tutor for O-level Biology',
+//     days: '5-Days / Week',
+//     budget: '20,000 PKR / Month',
+//     time: '1-Hour / Day',
+//   },
+//   {
+//     title: 'Need an experienced female tutor for O-level Biology',
+//     days: '5-Days / Week',
+//     budget: '20,000 PKR / Month',
+//     time: '1-Hour / Day',
+//   },
+//   {
+//     title: 'Need an experienced female tutor for O-level Biology',
+//     days: '5-Days / Week',
+//     budget: '20,000 PKR / Month',
+//     time: '1-Hour / Day',
+//   },
+//   {
+//     title: 'Need an experienced female tutor for O-level Biology',
+//     days: '5-Days / Week',
+//     budget: '20,000 PKR / Month',
+//     time: '1-Hour / Day',
+//   },
+//   {
+//     title: 'Need an experienced female tutor for O-level Biology',
+//     days: '5-Days / Week',
+//     budget: '20,000 PKR / Month',
+//     time: '1-Hour / Day',
+//   },
+// ];
+
+// export default function WritingAnnouncements(props) {
+//   const [announcements, setAnnouncements] = useState(data);
+
+//   const fetchMoreData = () => {
+//     setTimeout(() => {
+//       setAnnouncements([...announcements, ...data]);
+//     }, 500);
+//   };
+
+//   return (
+//     <div
+//       className={styles.announcements}
+//       style={{ margin: '0 auto', width: '70%' }}
+//     >
+//       <InfiniteScroll
+//         dataLength={announcements.length}
+//         next={fetchMoreData}
+//         hasMore={true}
+//         height={500}
+//         loader={<h4>Loading...</h4>}
+//       >
+//         {announcements.map((announcement, index) => (
+//           <Announcement key={index} announcement={announcement} />
+//         ))}
+//       </InfiniteScroll>
+//     </div>
+//   );
+// }
