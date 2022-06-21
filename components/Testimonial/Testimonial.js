1b2a9b4f07bb9992d389253cd02db2c6c8574556
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-06-21 22:34:26 +0500

    minor changes

--- components/Testimonial/Testimonial.js
+++ components/Testimonial/Testimonial.js
@@ -3,12 +3,24 @@ import styles from './Testimonial.module.css';
 export default function Testimonial(props) {
   return (
     <div className={styles.testimonial}>
-      <p className={styles.description}>
-        <span>“</span>I just wanted to share a quick note and let you know that
-        you guys do a really good job. I'm glad I decided to work with you. It's
-        really great how easy your websites are to update and manage.
-      </p>
-      <p className={styles.username}>- Some Guy</p>
+      <div>Image</div>
+      <div>
+        <p className={styles.username}>Christine Stewart</p>
+        <div className={styles.rating}>
+          {[...Array(3)].map(() => (
+            <span>star1</span>
+          ))}
+          {[...Array(5 - 3)].map(() => (
+            <span>star2</span>
+          ))}
+        </div>
+        <p className={styles.description}>
+          <span>“</span>I just wanted to share a quick note and let you know
+          that you guys do a really good job. I'm glad I decided to work with
+          you. It's really great how easy your websites are to update and
+          manage.
+        </p>
+      </div>
     </div>
   );
 }
--- components/Testimonials/Testimonials.js
+++ components/Testimonials/Testimonials.js
@@ -5,7 +5,12 @@ export default function Testimonials(props) {
   return (
     <div className={styles.testimonials}>
       {[...Array(5)].map((testimonial, index) => (
-        <Testimonial key={index} />
+        <Testimonial
+          // image={testimonial.image}
+          // username={testimonial.username}
+          // rating={testimonial.rating}
+          // description={testimonial.description}
+        />
       ))}
     </div>
   );
