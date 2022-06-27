7a9e5b6ea812d5a7c64425fa8b88ba333306c842
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-06-27 21:19:22 +0500

    added new review modal in reviews

--- pages/admin/approvereviews.js
+++ pages/admin/approvereviews.js
@@ -33,6 +33,7 @@ export async function getServerSideProps() {
           rating: 4,
           description:
             "I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you. It's really great how easy your websites are to update and managte.",
+          isApproved: false,
         },
         {
           image: 'f1',
@@ -40,6 +41,7 @@ export async function getServerSideProps() {
           rating: 4,
           description:
             "I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you. It's really great how easy your websites are to update and managte.",
+          isApproved: false,
         },
         {
           image: 'f1',
@@ -47,6 +49,7 @@ export async function getServerSideProps() {
           rating: 4,
           description:
             "I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you. It's really great how easy your websites are to update and managte.",
+          isApproved: false,
         },
         {
           image: 'f1',
@@ -54,6 +57,7 @@ export async function getServerSideProps() {
           rating: 4,
           description:
             "I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you. It's really great how easy your websites are to update and managte.",
+          isApproved: false,
         },
         {
           image: 'f1',
@@ -61,6 +65,7 @@ export async function getServerSideProps() {
           rating: '4',
           description:
             "I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you. It's really great how easy your websites are to update and managte.",
+          isApproved: false,
         },
       ],
     },
