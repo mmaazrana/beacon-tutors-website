8ce5a2705e5d92de891a6daf9205c6f838c240a8
mmaazrana <awaismaaz@gmail.com>
2022-06-28 12:50:21 +0500

    made all imports dynamic, made terms and conditon responsive

--- components/Layouts/Layout.js
+++ components/Layouts/Layout.js
@@ -1,16 +1,24 @@
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
-import { ReactComponent as Pattern } from '../../assets/background pattern.svg';
+import {ReactComponent as Pattern} from '../../assets/background pattern.svg';
+import {motion} from "framer-motion"
 
-export default function Layout({ children }) {
-  return (
-    <>
-      <div className={'background'}>
-        <Pattern viewBox="0 0 100% 0" />
-      </div>
-      <Nav className="nav" />
-      <main>{children}</main>
-      <Footer />
-    </>
-  );
+export default function Layout({children}) {
+
+
+    return (<>
+            {/*<motion.div style={{opacity: 0}}*/}
+            {/*    animate = {{opacity: 1}}*/}
+            {/*    transition = {{duration: 0.3}}*/}
+            {/*>*/}
+                <div className = {'background'}>
+                    <Pattern viewBox = "0 0 100% 0"/>
+                </div>
+                <Nav className = "nav"/>
+                <main>{children}</main>
+                <Footer/>
+            {/*</motion.div>*/}
+        </>
+
+    );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,47 @@
 import '../styles/globals.css';
 import Head from 'next/Head';
 // import { ReactComponent as Pattern } from '../assets/background pattern.svg';
-import PerfectScrollbar from 'react-perfect-scrollbar';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import dynamic from "next/dynamic";
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+import Loading from "../components/Loading/Loading";
 
-function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => page);
+function MyApp({Component, pageProps}) {
 
-  const Pattern = dynamic(() => import('../assets/background pattern.svg').then(module => module.ReactComponent),);
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
 
-  return getLayout(
-    <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="keywords" content="titla, meta, nextjs" />
-        <meta name="author" content="Syamlal CM" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Beacon Tutors Pakistan</title>
-      </Head>
-      <div className={'background'}>
-        <Pattern viewBox="0 0 100% 0" />
-      </div>
-      {/* <PerfectScrollbar> */}
-        <Component className={'body'} {...pageProps} />
-      {/* </PerfectScrollbar> */}
-    </>
-  );
+    useEffect(() => {
+        const handleStart = (url) => {
+            url !== router.pathname ? setLoading(true) : setLoading(false);
+        };
+        const handleComplete = (url) => setLoading(false);
+        router.events.on("routeChangeStart", handleStart);
+        router.events.on("routeChangeComplete", handleComplete);
+        router.events.on("routeChangeError", handleComplete);
+    }, [router]);
+
+    const getLayout = Component.getLayout || ((page) => page);
+
+    const Pattern = dynamic(() => import('../assets/background pattern.svg').then(module => module.ReactComponent),);
+
+    return getLayout(
+        <>
+            <Head>
+                <meta charSet = "UTF-8"/>
+                <meta name = "keywords" content = "titla, meta, nextjs"/>
+                <meta name = "author" content = "Syamlal CM"/>
+                <meta name = "viewport" content = "width=device-width, initial-scale=1.0"/>
+                <title>Beacon Tutors Pakistan</title>
+            </Head>
+            <div className = {'background'}>
+                <Pattern viewBox = "0 0 100% 0"/>
+            </div>
+            <Loading loading = {loading}/>
+            <Component className = {'body'} {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
