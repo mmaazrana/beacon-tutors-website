35322ab9080f559deb8386ed5040f0f4e03e3b7b
mmaazrana <awaismaaz@gmail.com>
2022-07-06 23:58:16 +0500

    modified thank you page

--- components/ThankYou/ThankYou.js
+++ components/ThankYou/ThankYou.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import styles from './ThankYou.module.css'
-
+import Lottie from "lottie-react";
+import ThankYouLottie from '../../assets/lotties/thank-you.json'
+import FadeAnimationsStyles from "../../styles/Animations/FadeAnimations.module.css";
+import PopupAnimations from "../../styles/Animations/ScaleFadeDelayedAnimations.module.css";
+import {CSSTransition} from "react-transition-group";
+import {useRouter} from "next/router";
+import Link from "next/link";
+import dynamic from "next/dynamic";
+import {PhoneCall} from 'lucide-react'
 export default function ThankYou(props) {
-    return (
-        <>
-            <h1>
-                Tysm bro
-            </h1>
-        </>
+    const router = useRouter();
+
+    const MobileView = dynamic(() =>
+        import('react-device-detect').then((module) => module.MobileView)
     );
+    const WhatsappSvg = dynamic(() =>
+        import('../../assets/whatsapp.svg').then((module) => module.ReactComponent)
+    );
+    return (
+        <CSSTransition
+            key = {router.pathname}
+            in = {true}
+            appear = {true}
+            timeout = {500}
+            unmountOnExit
+            classNames = {FadeAnimationsStyles}>
+        <div className = {styles.wrapper}>
+            <Lottie className = {styles.lottie} loop= {0}  animationData = {ThankYouLottie}
+                    autoplay = {true} initialSegment={[0,120]}/>
+            <CSSTransition
+                key = {router.pathname}
+                in = {true}
+                appear = {true}
+                timeout = {750}
+                classNames = {PopupAnimations}>
+                <div className = {styles.contact}>
+            <MobileView className = {styles.contact}>
+                <Link href = {'/'}><a className={styles.primaryButton} ><WhatsappSvg/>Contact Us</a></Link>
+                <Link href = {'/'}><a className={styles.secondaryButton} ><PhoneCall/></a></Link>
+                </MobileView>
+                </div>
+
+            </CSSTransition>
+
+        </div>
+
+
+    </CSSTransition>
+);
 }
 
