a5c0847e8aaf7b6142291b59140e7d2d3a3d96c0
mmaazrana <awaismaaz@gmail.com>
2022-08-19 05:15:47 +0500

    fixed bug

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-
+import Script from 'next/script';
 class MyDocument extends Document {
   render() {
     return (
