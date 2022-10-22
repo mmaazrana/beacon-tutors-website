f2c205780ea14e60fd3d5450d908c1a9bfe95381
Muhammad Maaz <62180003+mmaazrana@users.noreply.github.com>
2022-10-23 03:28:37 +0500

    changed nav button to contact us

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -69,7 +69,7 @@ export default function Nav() {
                         link = {'/work'} text = {'Work as a Tutor'}/> <LinkButton link = {'/terms'}
                                                                                   text = {'Terms & Conditions'}/>
                     </div>
-                    <LinkButton nav = {true} link = {'/identity'} text = {'Submit Inquiry'}/>
+                    <LinkButton nav = {true} link = {'/selectcity'} text = {'Contact Us'}/>
                 </div>
             </div>
 
