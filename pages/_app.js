a876f927e29f1b9b50efac16749fffdcc28baea4
Muhammad Maaz <62180003+mmaazrana@users.noreply.github.com>
2022-10-25 19:20:41 +0500

    removed google ads tags

--- pages/_app.js
+++ pages/_app.js
@@ -34,23 +34,23 @@ function MyApp({Component, pageProps}) {
     const getLayout = Component.getLayout || ((page) => page);
 
     return getLayout(<>
-        <Script
-            id="gtm-script"
-            strategy = "afterInteractive"
-            dangerouslySetInnerHTML = {{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W6Q2KQK');`,
-            }}
-        />
-        <noscript
-            dangerouslySetInnerHTML = {{
-                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6Q2KQK"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-        />
+{/*        <Script*/}
+{/*            id="gtm-script"*/}
+{/*            strategy = "afterInteractive"*/}
+{/*            dangerouslySetInnerHTML = {{*/}
+{/*                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':*/}
+{/*new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],*/}
+{/*j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=*/}
+{/*'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);*/}
+{/*})(window,document,'script','dataLayer','GTM-W6Q2KQK');`,*/}
+{/*            }}*/}
+{/*        />*/}
+{/*        <noscript*/}
+{/*            dangerouslySetInnerHTML = {{*/}
+{/*                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6Q2KQK"*/}
+{/*height="0" width="0" style="display:none;visibility:hidden"></iframe>`,*/}
+{/*            }}*/}
+{/*        />*/}
         <Head>
 
             <meta charSet = "UTF-8"/>
